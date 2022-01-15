a55b93d3e8908a2e92e6028a40fbb1f2c61b18c2
Julia Foucher <julia814@hotmail.fr>
2022-01-15 14:16:57 +0100

    exo 2 done

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-#lire toutes les tasks
-# get 'tasks', to: 'tasks#index'
-# #lire une task
-# get 'tasks/:id', to: 'tasks#show'
+ #lire toutes les tasks
+get 'tasks', to: 'tasks#index', as: :tasks
 # #créer une task
-# get 'tasks/new', to: 'tasks#new'
-# gost 'tasks', to: 'tasks#create'
+get 'tasks/new', to: 'tasks#new', as: :new
+post 'tasks', to: 'tasks#create'
+#lire une task
+get 'tasks/:id', to: 'tasks#show', as: :task
 # #updater une task
-# get 'tasks/:id/edit', to: 'tasks#edit'
-# patch 'tasks/:id', to: 'tasks#update'
+get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+patch 'tasks/:id', to: 'tasks#update'
 # #supprimer un resto
-# delete 'tasks/:id', to: 'tasks#destroy'
+delete 'tasks/:id', to: 'tasks#destroy', as: :delete
 
 end
