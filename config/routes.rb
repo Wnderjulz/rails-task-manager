2ce0b29f36cafeb69caddcb24b9dc18bd0f2917c
Julia Foucher <julia814@hotmail.fr>
2022-01-15 11:36:04 +0100

    exo 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 #lire toutes les tasks
-get 'tasks', to: 'tasks#index'
+# get 'tasks', to: 'tasks#index'
 # #lire une task
 # get 'tasks/:id', to: 'tasks#show'
 # #créer une task
